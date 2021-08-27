4607c8ee205e92f790b2988f5bd6dd5deb5ff1d0
vn0v3ld <Ipconfig@22>
2021-08-27 01:25:42 -0700

    cron files

--- cron_light_on.py
+++ cron_light_on.py
@@ -1,5 +1,5 @@
-from ..IO.relay import Relay
-from ..models.relay_device import RelayStateEnum
+from IO.relay import Relay
+from models.relay_device import RelayStateEnum
 
 if __name__ == "__main__":
     Relay().change_light_relay_state(RelayStateEnum.ENABLED.value)
