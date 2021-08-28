90a55be00ca7bbdccc1d3c83cf7c3efcf179bbcf
vn0v3ld <Ipconfig@22>
2021-08-28 00:44:20 -0700

    cron files

--- cron_light_on.py
+++ cron_light_on.py
@@ -1,18 +1,8 @@
 from IO.relay import Relay
 from models.relay_device import RelayStateEnum
-import time
 
-import sys
-sys.path.append("/home/pi/Kraken/kraken-sensor")
 
 if __name__ == "__main__":
     relay = Relay()
-    aqui = relay.read_light_relay_state()
-    print(aqui)
-
-    if relay.read_light_relay_state() == RelayStateEnum.ENABLED.value:
-        print("ativo")
-    else:
-        print("inativo")
+    if relay.read_light_relay_state() == RelayStateEnum.DISABLED.value:
         relay.change_light_relay_state(RelayStateEnum.ENABLED.value)
-
