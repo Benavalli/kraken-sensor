554e459695a0650ee4774cf1d8155432154f0960
Benavalli <benavalli@gmail.com>
2021-08-30 15:42:30 -0700

    temperature cron job (#2)
    Co-authored-by: vn0v3ld

--- cron/cron_pump_on.py
+++ cron/cron_pump_on.py
@@ -5,4 +5,3 @@ if __name__ == "__main__":
     relay = Relay()
     if relay.read_pump_relay_state() == RelayStateEnum.DISABLED.value:
         relay.change_pump_relay_state(RelayStateEnum.ENABLED.value)
-        
