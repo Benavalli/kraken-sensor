02f6ef1599e26465e296679f52cd383331edf03e
Benavalli <benavalli@gmail.com>
2021-10-01 17:48:07 -0700

    Feature/temperature service (#3)
    * temperature cron job
    
    * temperature cron job
    
    * cron improvements

--- cron/cron_temperature_humidity.py
+++ cron/cron_temperature_humidity.py
@@ -7,22 +7,22 @@ if __name__ == "__main__":
     relay = Relay()
     temperature_humidity = dht11.get_temperature_humidity()
 
-    if temperature_humidity.humidity < 45:
+    if temperature_humidity.humidity < 41:
         if relay.read_humidifier_relay_state() == RelayStateEnum.DISABLED.value:
             relay.change_humidifier_relay_state(RelayStateEnum.ENABLED.value)
-        if relay.read_exhaust_relay_state() == RelayStateEnum.ENABLED.value and temperature_humidity.celsius < 27:
-            relay.change_exhaust_relay_state(RelayStateEnum.DISABLED.value)
+        #if relay.read_exhaust_relay_state() == RelayStateEnum.ENABLED.value and temperature_humidity.celsius < 27:
+            #relay.change_exhaust_relay_state(RelayStateEnum.DISABLED.value)
 
-    if temperature_humidity.humidity > 65:
+    if temperature_humidity.humidity > 52:
         if relay.read_humidifier_relay_state() == RelayStateEnum.ENABLED.value:
             relay.change_humidifier_relay_state(RelayStateEnum.DISABLED.value)
-        if relay.read_exhaust_relay_state() == RelayStateEnum.DISABLED.value:
-            relay.change_exhaust_relay_state(RelayStateEnum.ENABLED.value)
+        #if relay.read_exhaust_relay_state() == RelayStateEnum.DISABLED.value:
+            #relay.change_exhaust_relay_state(RelayStateEnum.ENABLED.value)
 
-    if temperature_humidity.celsius < 27:
-        if relay.read_exhaust_relay_state() == RelayStateEnum.ENABLED.value and temperature_humidity.humidity < 65:
+    if temperature_humidity.celsius < 24:
+        if relay.read_exhaust_relay_state() == RelayStateEnum.ENABLED.value and temperature_humidity.humidity < 52 and relay.read_light_state() == RelayStateEnum.DISABLED.value:
             relay.change_exhaust_relay_state(RelayStateEnum.DISABLED.value)
 
-    if temperature_humidity.celsius > 29:
+    if temperature_humidity.celsius > 27:
         if relay.read_exhaust_relay_state() == RelayStateEnum.DISABLED.value:
             relay.change_exhaust_relay_state(RelayStateEnum.ENABLED.value)
