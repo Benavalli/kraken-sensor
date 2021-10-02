1fd9c182f88dc8fc46d9b7a225762fc33970ca5b
vn0v3ld <Ipconfig@22>
2021-10-01 18:56:30 -0700

    cron temperature change

--- IO/relay.py
+++ IO/relay.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 import configparser
-import json
 import os
 
 from models.relay_device import RelayDeviceEnum, RelayDevice, RelayStateEnum
@@ -108,5 +107,10 @@ class Relay(object):
         GPIO.output(self.relay_pump_device.pin, state)
 
     def get_device_list(self):
-        return json.dumps([self.relay_light_device, self.relay_exhaust_device, self.relay_humidifier_device,
-                           self.relay_pump_device])
+        relays = [
+            self.relay_light_device,
+            self.relay_exhaust_device,
+            self.relay_humidifier_device,
+            self.relay_pump_device
+        ]
+        return dict.fromkeys(relays, "relay")
--- server/server.py
+++ server/server.py
@@ -15,7 +15,7 @@ def get_temperature_humidity():
 @app.route('/relays', methods=['GET'])
 def get_relays():
     relay_list = Relay().get_device_list()
-    return jsonify({'relays': relay_list})
+    return jsonify({'relays': relay_list.__dict__})
 
 
 @app.route('/light-relay-state', methods=['POST'])
