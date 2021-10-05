ec05ea01aba77e8f4203ac56b26b82b4465b8fa9
Benavalli <benavalli@gmail.com>
2021-10-05 11:56:09 -0700

    Fix/cron routines (#5)
    * getting enum string value
    
    * fixing cron
    
    Co-authored-by: vn0v3ld <Ipconfig@22>

--- cron/cron_temperature_humidity.py
+++ cron/cron_temperature_humidity.py
@@ -9,19 +9,19 @@ if __name__ == "__main__":
 
     if temperature_humidity.humidity < 41:
         if relay.read_humidifier_relay_state() == RelayStateEnum.DISABLED.value:
-            relay.change_humidifier_relay_state(RelayStateEnum.ENABLED.value)
+            relay.change_humidifier_relay_state(RelayStateEnum.ENABLED.name)
 
     if temperature_humidity.humidity > 52:
         if relay.read_humidifier_relay_state() == RelayStateEnum.ENABLED.value:
-            relay.change_humidifier_relay_state(RelayStateEnum.DISABLED.value)
+            relay.change_humidifier_relay_state(RelayStateEnum.DISABLED.name)
         if relay.read_exhaust_relay_state() == RelayStateEnum.DISABLED.value:
-            relay.change_exhaust_relay_state(RelayStateEnum.ENABLED.value)
+            relay.change_exhaust_relay_state(RelayStateEnum.ENABLED.name)
 
     if temperature_humidity.temp < 24:
         if relay.read_exhaust_relay_state() == RelayStateEnum.ENABLED.value and temperature_humidity.humidity < 52 \
                 and relay.read_light_state() == RelayStateEnum.DISABLED.value:
-            relay.change_exhaust_relay_state(RelayStateEnum.DISABLED.value)
+            relay.change_exhaust_relay_state(RelayStateEnum.DISABLED.name)
 
     if temperature_humidity.temp > 27:
         if relay.read_exhaust_relay_state() == RelayStateEnum.DISABLED.value:
-            relay.change_exhaust_relay_state(RelayStateEnum.ENABLED.value)
+            relay.change_exhaust_relay_state(RelayStateEnum.ENABLED.name)
