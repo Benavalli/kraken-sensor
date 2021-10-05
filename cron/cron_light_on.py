a877e6d91dcad31b39716bc14a609ec10f24e081
vn0v3ld <Ipconfig@22>
2021-10-05 11:54:54 -0700

    fixing cron

--- cron/cron_light_on.py
+++ cron/cron_light_on.py
@@ -4,5 +4,5 @@ from models.relay_device import RelayStateEnum
 if __name__ == "__main__":
     relay = Relay()
     if relay.read_light_relay_state() == RelayStateEnum.DISABLED.value:
-        relay.change_light_relay_state(RelayStateEnum.ENABLED.value)
-        relay.change_exhaust_relay_state(RelayStateEnum.ENABLED.value)
+        relay.change_light_relay_state(RelayStateEnum.ENABLED.name)
+        relay.change_exhaust_relay_state(RelayStateEnum.ENABLED.name)
--- cron/cron_pump_off.py
+++ cron/cron_pump_off.py
@@ -4,4 +4,4 @@ from models.relay_device import RelayStateEnum
 if __name__ == "__main__":
     relay = Relay()
     if relay.read_pump_relay_state() == RelayStateEnum.ENABLED.value:
-        relay.change_pump_relay_state(RelayStateEnum.DISABLED.value)
+        relay.change_pump_relay_state(RelayStateEnum.DISABLED.name)
