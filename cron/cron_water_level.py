67d00b4bc026641446d771b8aa8c2d05e64f1c30
Pedro Benavalli <pedro.benavalli@walmartlabs.com>
2022-09-06 15:16:26 -0700

    water level cron routine

--- cron/cron_water_level.py
+++ cron/cron_water_level.py
@@ -1,8 +1,26 @@
+from IO.relay import Relay
 from IO.sen0205 import Sen0205
 import time
 
+from models.relay_device import RelayStateEnum
+from models.water_level import WaterLevelEnum
+
 if __name__ == "__main__":
     water_level_sensor = Sen0205()
+    relay = Relay()
+
     while True:
-        print(water_level_sensor.get_water_level_sensor_state())
-        time.sleep(1.0)
+        try:
+            if water_level_sensor.get_water_level_sensor_state() == WaterLevelEnum.LOW.value and \
+                    relay.read_valve_relay_state() == RelayStateEnum.DISABLED.value:
+                relay.change_valve_relay_state(RelayStateEnum.ENABLED.name)
+
+            if water_level_sensor.get_water_level_sensor_state() == WaterLevelEnum.HIGH.value and \
+                    relay.read_valve_relay_state() == RelayStateEnum.ENABLED.value:
+                relay.change_valve_relay_state(RelayStateEnum.DISABLED.name)
+                break
+
+            time.sleep(0.3)
+
+        except:
+            break
---
+++ models/water_level.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class WaterLevelEnum(enum.Enum):
+    LOW = 0
+    HIGH = 1
