635723705a9d47ec1515b67c926921c3ad7d39b6
Pedro Benavalli <benavalli@gmail.com>
2023-08-25 00:14:18 -0700

    database integration

--- cron/test.py
+++
@@ -1,7 +0,0 @@
-from database import database
-from datetime import datetime
-from database.tables import Measures
-
-engine = database.get_engine()
-measure = Measures(date = datetime.now(), temperature = 23.4, humidity = 22.0)
-database.persist_object(measure)
