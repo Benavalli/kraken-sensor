8a63b12446c57f7461e8782106ffd5cf06661239
Pedro Benavalli <benavalli@gmail.com>
2023-08-27 15:41:17 -0700

    stream camera version

---
+++ IO/camera/pi_camera.py
@@ -0,0 +1,62 @@
+from time import sleep
+from datetime import datetime
+from data import db_manager
+from data.db_tables import Pictures
+from base_camera import BaseCamera
+
+import picamera
+import os
+import io
+
+
+class Camera(BaseCamera):
+    
+    @staticmethod
+    def frames():
+        with picamera.PiCamera() as camera:
+            # let camera warm up
+            sleep(5)
+
+            stream = io.BytesIO()
+            for foo in camera.capture_continuous(stream, 'jpeg',
+                                                 use_video_port=True):
+                # return current frame
+                stream.seek(0)
+                yield stream.read()
+
+                # reset stream for next frame
+                stream.seek(0)
+                stream.truncate()
+
+    def __persist_picture_object(blob_picture):
+        engine = db_manager.get_engine()
+        picture_object = Pictures(
+            date = datetime.now(), 
+            picture = blob_picture
+        )
+        db_manager.persist_object(engine, picture_object)
+    
+    def __delete_picture_file(file_name):
+        if os.path.isfile(file_name):
+            os.remove(file_name)
+        
+    def take_picture():
+        camera = picamera.PiCamera()
+        print('Preparing Camera...')
+        sleep(5)
+        print('Taking Picture...')
+        
+        file_name = '{}/taken-pictures/{}.jpg'.format(
+            os.path.dirname(__file__),
+            datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
+        )
+        
+        camera.capture(file_name)
+        camera.close()
+        return file_name
+    
+    def save_picture(file_name):
+        with open(file_name, 'rb') as file:
+            blob_picture = file.read()
+        __persist_picture_object(blob_picture) 
+        __delete_picture_file(file_name)     
--- cron/cron_camera.py
+++ cron/cron_camera.py
@@ -1,4 +1,4 @@
-from IO import camera
+from IO.camera import camera
 
 if __name__ == "__main__":
 	picture_file_name = camera.take_picture()
